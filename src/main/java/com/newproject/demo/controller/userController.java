cbebeee12b21d78c17143aac2be18ad6461badcd
Hiruna Gayashan <64424930+Hirunagrad@users.noreply.github.com>
2022-01-27 15:44:52 +0530

    add project

---
+++ src/main/java/com/newproject/demo/controller/userController.java
@@ -0,0 +1,17 @@
+package com.newproject.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+public class userController {
+
+    @GetMapping("/getuser")
+    public String getUser(){
+        return "Hirunagrad";
+    }
+}
---
+++ src/test/java/com/newproject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
