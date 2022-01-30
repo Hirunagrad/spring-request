71b714cbda3c1b1b7498f46bad55d17afae0f1b7
Hiruna Gayashan <64424930+Hirunagrad@users.noreply.github.com>
2022-01-30 15:39:46 +0530

    complete user data save equest

--- src/main/java/com/newproject/demo/controller/userController.java
+++ src/main/java/com/newproject/demo/controller/userController.java
@@ -1,20 +1,29 @@
 package com.newproject.demo.controller;
 
+import com.newproject.demo.dto.UserDTO;
+import com.newproject.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class userController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/getuser")
-    public String getUser(){
-        return "Hirunagrad";
+    public String getUser()
+    {
+       return "getUser";
+
     }
 
     @PostMapping("/saveuser")
-    public String saveUser(){
-        return "saveHiruna";
+    public UserDTO saveUser(@RequestBody UserDTO userDTO)
+    {
+        return userService.saveUser(userDTO);
+
     }
 
     @PutMapping("/updateuser")
